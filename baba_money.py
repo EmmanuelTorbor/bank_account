5cdf288d33f68c71f3ec94970233ee77fdfe7a0f
EmmanuelTorbor <emmanuel.torbor@hgs.hiddengeniusproject.org>
2020-12-08 15:22:01 -0800

    added baba_money.py

---
+++ baba_money.py
@@ -0,0 +1,21 @@
+
+
+p =  float(input("What's the principle "))
+r = float(input("Whats the rate "))
+n = int(input("How many periods "))
+t =  int(input('how many payments per period'))
+pv = p * (pow((1 + r/100/n),n * t))
+
+print(pv)
+
+
+
+
+
+def compound_interest(p,r,n,nt):
+    balance = p * (pow((1 + r/100/n),n*t))
+    ci = balance - p
+    print('Balance at the end of the period ',round(balance,2))
+    print("Interest earned is ", round(ci,2))
+
+compound_interest(5000,10,12,5)
