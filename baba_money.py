a0b92669b32c8ebefcf06bc374553fa0e18e38a5
EmmanuelTorbor <emmanuel.torbor@hgs.hiddengeniusproject.org>
2020-12-09 18:16:23 -0800

    finalized the function

--- baba_money.py
+++ baba_money.py
@@ -1,21 +1,25 @@
 
 
-p =  float(input("What's the principle "))
-r = float(input("Whats the rate "))
-n = int(input("How many periods "))
-t =  int(input('how many payments per period'))
+"""
 pv = p * (pow((1 + r/100/n),n * t))
 
 print(pv)
+"""
 
 
 
 
-
-def compound_interest(p,r,n,nt):
+def compound_interest():
+    p =  float(input("What's the principle "))
+    r = float(input("Whats the rate "))
+    n = int(input("How many periods "))
+    t =  int(input('how many payments per period'))
+    
     balance = p * (pow((1 + r/100/n),n*t))
     ci = balance - p
-    print('Balance at the end of the period ',round(balance,2))
-    print("Interest earned is ", round(ci,2))
+    print('Your balance at the end of the period will be $',round(balance,2),| "\n")
+    print('You earned $', round(ci,2), 'during the 1 year period.')
+    roi = (ci/balance) * 100
+    print('This is ', round(roi,2), '% return on investment')
 
-compound_interest(5000,10,12,5)
+compound_interest()
